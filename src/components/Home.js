23a13c7ed29c7b813debfcc9a9fd14e4eecc8904
carlita-capocci <carla.a.capocci@gmail.com>
2020-11-02 09:03:30 -0300

    cambios

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home(props){
     return <div> 
         <h1 style={{fontFamily:'arial', fontStyle: 'italic', fontSize:'30px' }}>{props.greeting} </h1>
         <div>{props.children} </div>
-        <div> <ItemCount stock= {5} initial={0} /> </div>
+        <div> <ItemCount stock={4} initial={0} onAdd={console.log()}/> </div>
 
      </div>
 
--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,21 +1,26 @@
 import React,{useState} from 'react'
-
+   
 function ItemCount({stock, initial, onAdd}){
     const [valor, setValor]= useState(0);
    
-return <div> <button onClick= {()=> {
-    if(valor<stock){
-    setValor( valor + 1)
-    }
-}} style= {{margin: "5px"}}> + </button>
-
-{valor}
+return <div> 
 
 <button onClick= {()=> {
     if(valor> 0 && valor<=stock){
     setValor( valor - 1)
     }
 }} style= {{margin: "5px"}} > - </button> 
+
+{valor}
+
+
+    <button onClick= {()=> {
+    if(valor<stock){
+    setValor( valor + 1)
+    }
+}} style= {{margin: "5px"}}> + </button>
+
+
 
 <div> <button onClick={onAdd} style= {{width: "120px", height:"20px", margin: "5px" }}> Agregar </button> </div>
 
